5858a4db130f7faf8beb034eaffefee7aa7d1548
Lucas <lucas.capanelli.es@gmail.com>
2021-02-08 23:29:18 -0300

    new vigencia backend return

--- api/vida/controllers/vida.js
+++ api/vida/controllers/vida.js
@@ -1,8 +1,19 @@
 'use strict';
-
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  find: ctx => {
+    return strapi.query('vida').find(ctx.query, [
+      {
+        path: 'category',
+        populate: {
+          path: 'vigencia',
+        },
+      },
+    ]);
+  },
+};
